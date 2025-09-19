ac835e42575adabc1d89dce6f812bd3a64e4df3a
serpuhovichok <serpuhovichok@yandex.ru>
2025-09-19 17:09:12 +0300

    HW level_1

--- django_templates_homework/views/level_1/a_phone_number.py
+++ django_templates_homework/views/level_1/a_phone_number.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
@@ -14,6 +13,4 @@ from django.shortcuts import render
 
 
 def get_phone_number_view(request):
-    phone_number = '+79848522383'
-
-    return HttpResponse(phone_number)  # код писать тут
+    return render(request, template_name='level_1/phone_number.html')
